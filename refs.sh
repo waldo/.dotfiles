7c3a7634ff25dcc2d7a624d2a2a60e613cd4ec19
Waldo <ben.walsham@meltingwaldo.com>
2012-05-03 18:08:09 +0100

    Force relinking option for reference dotfiles

--- refs.sh
+++ refs.sh
@@ -4,14 +4,13 @@ FILES=~/.dotfiles/refs/*
 shopt -s dotglob
 CURRENT_SHA=`GIT_DIR=~/.dotfiles/.git git log -n1 --format="%H"`
 
-if [ "$OLD_DOTFILES_SHA" != "$CURRENT_SHA" ]
+if [ "$OLD_DOTFILES_SHA" != "$CURRENT_SHA" -o "$1" == "-f" ]
 then
   for f in $FILES
   do
     echo "Processing $f file..."
-    ln -s $f ~
+    ln -s $1 $f ~
   done
 
   echo $CURRENT_SHA > ~/.dotfiles/sha.txt
-  clear
 fi
