e6c4d8ba0262a8c8c41ea3cbff8c5fcac5ca0489
Waldo <ben.walsham@meltingwaldo.com>
2020-07-15 11:15:44 +1000

    Fixes bash history saving

--- env.sh
+++ env.sh
@@ -2,10 +2,10 @@ export LC_CTYPE=en_US.UTF-8
 export CLICOLOR=true
 
 export EDITOR="code --wait"
-export HISTSIZE=100000
-export HISTFILESIZE=250000
+export HISTSIZE=25000
+export HISTFILESIZE=2500000
 shopt -s histappend
-PROMPT_COMMAND="history -a; history -c; history -r"
+PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
 
 export JAVA_HOME=/usr
 export SCRIPTS_HOME=~/proj/scripts
